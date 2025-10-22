abf22f723768cc67aa7ee7c9eff439720a2d614c
Ashishkumar18m <ashish.kumar.18m@gmail.com>
2025-10-22 19:03:00 +0530

    Print Name

---
+++ Lab_1/Q01.java
@@ -0,0 +1,10 @@
+package JAVA.Lab_1;
+
+public class Q01 {
+     public static void main(String args[]) {
+
+        System.out.println("Name: " + "Ashish");
+        System.out.println("Address: " + "Pataudi(HR)");
+
+    }
+}
